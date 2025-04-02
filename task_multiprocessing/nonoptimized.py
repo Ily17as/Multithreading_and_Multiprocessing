efc5a82df842dcd46d1540a8de74aa9606c23c5b
ILyas Galiev <ilyas_gal@internet.ru>
2025-04-02 18:42:27 +0300

    Update server.py, optimized.py, nonoptimized.py

--- task_multiprocessing/nonoptimized.py
+++ task_multiprocessing/nonoptimized.py
@@ -1,2 +1,19 @@
-# Add your solution here
+def check_prime(number):
+    if number < 2:
+        return False
+    for i in range(2, number):
+        if number % i == 0:
+            return False
+    return True
 
+
+def main():
+    filepath = "primes_nonoptimized.txt"
+    with open(filepath, "w") as file:
+        for i in range(1, 1000001):
+            if check_prime(i):
+                file.write(str(i) + "\n")
+
+
+if __name__ == "__main__":
+    main()
--- task_multiprocessing/optimized.py
+++ task_multiprocessing/optimized.py
@@ -1,2 +1,28 @@
-# Add your solution here
+from multiprocessing import Pool
+
+
+def check_prime(number):
+    if number < 2:
+        return False
+    for i in range(2, number):
+        if number % i == 0:
+            return False
+    return True
+
+
+def main():
+    filepath = "primes_optimized.txt"
+
+    with Pool() as pool:
+        numbers = list(range(1, 1000001))
+        results = pool.map(check_prime, numbers)
+
+    primes = [str(num) for num, is_prime in zip(numbers, results) if is_prime]
+
+    with open(filepath, "w") as f:
+        f.write("\n".join(primes))
+
+
+if __name__ == "__main__":
+    main()
 
